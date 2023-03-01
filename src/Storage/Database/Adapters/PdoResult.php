ade484dc88ef5361940479e6aa4d442b20ffc2c8
jake <bigjake2s@gmail.com>
2023-03-01 09:44:36 -0500

    fix: letting PDOStatement define DatabaseResultInterface methods

--- src/Storage/Database/Adapters/PdoResult.php
+++ src/Storage/Database/Adapters/PdoResult.php
@@ -8,36 +8,4 @@ use PDOStatement;
 
 class PdoResult extends PDOStatement implements DatabaseResultInterface
 {
-    public function fetch($how = null, $orientation = null, $offset = null)
-    {
-        if ($how === null) {
-            return parent::fetch();
-        }
-        if ($orientation === null) {
-            return parent::fetch($how);
-        }
-        if ($offset === null) {
-            return parent::fetch($how, $orientation);
-        }
-        return parent::fetch($how, $orientation, $offset);
-    }
-
-    public function fetchAll($how = null, $class = null, $args = null): array
-    {
-        if ($how === null) {
-            return parent::fetchAll();
-        }
-        if ($class === null) {
-            return parent::fetchAll($how);
-        }
-        if ($args === null) {
-            return parent::fetchAll($how, $class);
-        }
-        return parent::fetchAll($how, $class, $args);
-    }
-
-    public function fetchColumn($column = 0)
-    {
-        return parent::fetchColumn($column);
-    }
 }
